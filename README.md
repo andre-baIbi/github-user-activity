bd660d5503a8943f48fc769d1f32a8fe0baf330a
André Balbi <amabalbi@id.uff.br>
2025-12-26 13:05:57 -0300

    <feat/fix> v4 - README + error handling + minor fixes

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ https://roadmap.sh/projects/github-user-activity
 Run `py .\main.py <username> ` to see users events by type, e.g. `py .\main.py "kamranahmedse"`
 
 Showing information about:
+- WatchEvent
 - PushEvent
 - IssuesEvent
+- IssueCommentEvent
+- PullRequestEvent
+- ForkEvent
+
 
 [About GitHubAPI events](https://docs.github.com/en/enterprise-server@3.15/rest/using-the-rest-api/github-event-types?apiVersion=2022-11-28)
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from argparse import ArgumentParser
 
+from requests import HTTPError
+
 from core.ActivityUtils import getEventTypeDict, showEventsPerType, showActivityByEventType
 from core.GithubHandler import getEventsOfUserFromGithubApi
 
 
 def printActivityUseCase(username: str):
-    #1 : Map events
-    eventList = getEventsOfUserFromGithubApi(username)
+    try:
+        # 1 : Map events
+        eventList = getEventsOfUserFromGithubApi(username)
 
-    # 2: Load and show activity
-    eventTypeDict = getEventTypeDict(eventList)
-    print(f"{username}'s events:")
-    showEventsPerType(eventTypeDict)
-    showActivityByEventType(eventTypeDict) #todo me!
+        # 2: Load and show activity
+        eventTypeDict = getEventTypeDict(eventList)
+        print(f"{username}'s events:")
+        showEventsPerType(eventTypeDict)
+        showActivityByEventType(eventTypeDict)
+
+    except HTTPError as e:
+        if "404" in str(e):
+            print("[ERROR] Username was NOT found!")
+        else:
+            print(f"[ERROR] {e}")
 
 
 if __name__ == '__main__':
